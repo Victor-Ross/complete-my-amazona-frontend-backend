618f233d516963ae5b9800fb1d10d4edd39a45c6
Victor Ross <victor2ross@gmail.com>
2022-03-23 22:15:36 -0300

    plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sassDts from 'vite-plugin-sass-dts';
 
-declare module '*.scss';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sassDts()],
 });
