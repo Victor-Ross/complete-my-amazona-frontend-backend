f58e4cbf90001c77f42be8ab0e88e91a0f6fbf57
Victor Ross <victor2ross@gmail.com>
2022-03-23 23:10:56 -0300

    trust-basir

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,15 +10,11 @@ app.use(cors());
 
 const __dirname = path.resolve();
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.use(
-  express.static(
-    path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html')
-  )
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+});
 
 app.use(routes);
 
