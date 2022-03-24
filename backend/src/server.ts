2623b7cdfb0ac073c04e89ecdb02f0e835af35bf
Victor Ross <victor2ross@gmail.com>
2022-03-24 08:02:53 -0300

    tentativa

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
 app.use(routes);
