b52c2f255a6ba792cb3d30f5cee7e8e8af128ea5
Victor Ross <victor2ross@gmail.com>
2022-03-24 08:17:16 -0300

    dist build

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 var __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 app.use(index_1.routes);
 app.use(function (err, req, res, next) {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,10 +10,10 @@ app.use(cors());
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
 app.use(routes);
